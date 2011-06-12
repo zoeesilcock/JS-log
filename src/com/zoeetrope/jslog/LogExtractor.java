cd776aff4732febe961bdd23bf3d410df2bac567
zoee <mrzoee@gmail.com>
2011-06-12 12:12:16 +0200

    added the license information

--- src/com/zoeetrope/jslog/LogExtractor.java
+++ src/com/zoeetrope/jslog/LogExtractor.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2011 Zoee Silcock (zoeetrope.com)
+ * 
+ * This file is part of JSLog.
+ * 
+ * JSLog is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JSLog is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with JSLog.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.zoeetrope.jslog;
 
 import java.io.BufferedReader;
@@ -9,7 +27,6 @@ import java.util.regex.Pattern;
 public class LogExtractor implements Runnable {
 	
 	private NotificationService service;
-//	private ArrayList<JSLogItem> items = new ArrayList<JSLogItem>();
 	private int id = 0;
 	
 	public LogExtractor(NotificationService service) {
@@ -56,7 +73,6 @@ public class LogExtractor implements Runnable {
             		// Build the JSLogItem with the data extracted from the line.
             		JSLogItem item = new JSLogItem(this.id, icon, matcher.group(1), 
             				"Line: " + matcher.group(3) + " of " + matcher.group(2));
-//            		this.items.add(item);
 	            	service.displayItem(item);
 	        		
 	        		this.id++;
